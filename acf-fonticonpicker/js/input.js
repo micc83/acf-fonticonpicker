f1d8631a1eb2cb780c34368b4b522bf2c55a0653
Alessandro Benoit <micc83@gmail.com>
2015-09-11 15:54:03 +0200

    Update input.js

--- acf-fonticonpicker/js/input.js
+++ acf-fonticonpicker/js/input.js
@@ -1,19 +1,21 @@
 (function($){
-	if( typeof acf.add_action !== 'undefined' ) {
-		acf.add_action('ready append', function( $el ){
-			// Let's iconpick!!!
-			$('.acf-iconpicker').fontIconPicker();
-		});	
-	} else {
-		// ACF4
-		$(document).on('acf/include_fields', function(e, postbox){
-			$(postbox).find('.acf-iconpicker').each(function(){
-				// The check is made so repeaters wont trigger the script before time
-				if ( !$(this).parents('.row-clone').length ){
-					// Let's iconpick!!!
-					$(this).fontIconPicker();	
-				}	
-			});	
-		});
-	}
+    function enableFontIconPickerFor($el) {
+        $el.find('.acf-iconpicker').each(function(){
+            if ( !$(this).parents('.acf-clone').length ){
+                // Let's iconpick!!!
+                $(this).fontIconPicker();   
+            }
+        });
+    }
+    if( typeof acf.add_action !== 'undefined' ) {
+        // ACF5
+        acf.add_action('ready append', function( $el ){
+            enableFontIconPickerFor($el);
+        }); 
+    } else {
+        // ACF4
+        $(document).on('acf/include_fields', function(e, postbox){
+            enableFontIconPickerFor($(postbox));
+        });
+    }
 })(jQuery);
